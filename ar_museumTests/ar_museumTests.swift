14b4acc0f63c7b890528ebca6ba72f303ef89c8f
Conn Ho <2015954952@qq.com>
2025-03-22 18:29:59 +0800

    first commit

---
+++ ar_museumTests/ar_museumTests.swift
@@ -0,0 +1,17 @@
+//
+//  ar_museumTests.swift
+//  ar_museumTests
+//
+//  Created by ConnHo on 2025/3/22.
+//
+
+import Testing
+@testable import ar_museum
+
+struct ar_museumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
